4c52981980495fe36e5dbc92686848f4f2b35e8b
Jennifer Girard <jenn-suki@hotmail.com>
2020-01-19 09:02:56 -0500

    cards

--- dive/src/app/dive-feed/dive-feed.component.ts
+++ dive/src/app/dive-feed/dive-feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing/listing';
+
 
 @Component({
   selector: 'app-dive-feed',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiveFeedComponent implements OnInit {
 
-  constructor() { }
+  listings: Listing[] = [];
+
+  constructor() {
+    const list: Listing = new Listing('Amba', '1997-06-24 2:42 PM', 'Pile of trash', 'end of the wrold', 'decomposing', 'img1');
+    this.listings.push(list);
+    this.listings.push(new Listing('Bloc', '2000-12-11 2:41 PM', 'Hot dogs', 'The Stand', 'fresh', 'img2'));
+   }
 
   ngOnInit() {
+  }
+
+  onLike(listing: Listing) {
+    listing.like++;
   }
 
 }
--- dive/src/app/listing/listing.ts
+++ dive/src/app/listing/listing.ts
@@ -6,13 +6,15 @@ export class Listing {
   state: string;
   like: number;
   gps: string;
+  img: string;
 
-  constructor(author: string, date: string, product: string, location: string, state: string) {
+  constructor(author: string, date: string, product: string, location: string, state: string, img: string) {
     this.author = author;
     this.date = date;
     this.product = product;
     this.location = location;
     this.state = state;
     this.like = 0;
+    this.img = img;
   }
  }
